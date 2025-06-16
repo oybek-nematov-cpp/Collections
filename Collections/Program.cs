ab948445e53b52a074607f2439a2515928960ff8
Dell <Dell@DESKTOP-081C5OM>
2025-06-16 20:12:10 +0500

    Добавьте файлы проекта.

---
+++ Collections/Program.cs
@@ -0,0 +1,10 @@
+﻿List<string> meva = new List<string>();
+{
+    meva.Add("appale");
+    meva.Add("banana");
+    meva.Add("orange");
+}
+foreach (var item in meva)
+{
+    Console.WriteLine(item);
+}
